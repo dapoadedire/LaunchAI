6fb3c74d63c644b4046e615f7f967bdf9a7e6cde
Dapo Adedire <adedireadedapo19@gmail.com>
2024-03-31 15:33:11 +0100

    Refactor code to only open links

--- popup.js
+++ popup.js
@@ -2,9 +2,8 @@ const AIWebsites = {
     ChatGPT: "https://chat.openai.com",
     Gemini: "https://gemini.google.com",
     Mistral: "https://chat.mistral.ai",
-    'Claude.ai': "https://claude.ai",
-    'Bing AI': "https://www.bing.com/chat"
-
+    "Claude.ai": "https://claude.ai",
+    "Bing AI": "https://www.bing.com/chat",
 };
 
 const popupLinks = document.querySelector(".popup-links");
@@ -14,15 +13,8 @@ for (const [name, url] of Object.entries(AIWebsites)) {
     button.textContent = name;
     button.addEventListener("click", () => {
         chrome.tabs.query({ url }, (tabs) => {
-            alert("tabs", tabs);
-            if (tabs.length > 0) {
-                chrome.tabs.update(tabs[0].id, { active: true });
-            } else {
-                chrome.tabs.create({ url });
-            }
+            chrome.tabs.create({ url });
         });
     });
     popupLinks.appendChild(button);
 }
-
-
